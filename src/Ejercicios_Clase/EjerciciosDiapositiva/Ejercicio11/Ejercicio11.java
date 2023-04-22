137e8b4699b059dc9ec180e9f7ae54a8cde850ae
PabloMateosP <pmatpal0105@g.educaand.es>
2023-04-22 13:15:14 +0200

    Subida ejercicio 11 correcto prueba contactos daniel

--- src/Ejercicios_Clase/EjerciciosDiapositiva/Ejercicio11/Ejercicio11.java
+++ src/Ejercicios_Clase/EjerciciosDiapositiva/Ejercicio11/Ejercicio11.java
@@ -16,7 +16,7 @@ public class Ejercicio11 {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
 
         // Cargamos fichero que vamos a leer
-        File file = new File("Ejercicios_Clase/EjerciciosDiapositiva/Ejercicio11/numeros.xml");
+        File file = new File("C:\\Users\\pablo\\Desktop\\Tema7\\src\\Ejercicios_Clase\\EjerciciosDiapositiva\\Ejercicio11\\ContactosDaniel.xml");
         //IMPORTANTE: Hay que poner la ruta de darlo al botón derecho encima del archivo y donde pone copy path
         // --> path from source root
 
@@ -31,21 +31,34 @@ public class Ejercicio11 {
             //Nos dará el nombre del nodo en un caso estudiante
             System.out.println("Tipo de node: " + document.getDocumentElement().getNodeType());
             //Nos dará de tipo 1, ya que es un elemento
+            System.out.println("----------");
 
             // Accedemos a todos los nodos con el tag "estudiante" --> En la práctica 7 podemos sacar las participaciones de esta forma
             NodeList nombre = document.getElementsByTagName("nombre");
+            NodeList apellidos = document.getElementsByTagName("apellidos");
+            NodeList direccion = document.getElementsByTagName("direccion");
 
             //Con el: estudiantes.GetChildNodes para sacar el hijo de estudiante
 
             // Recorremos todos esos nodos
-            // Para la práctica 7 debemos de meter además de sacar el nombre del alumno sacar la participación de este.
             for (int i = 0; i < nombre.getLength(); i++) {
                 Node nodeContacto = nombre.item(i);
+                Node nodeContactoap = apellidos.item(i);
+                Node nodeContactodic = direccion.item(i);
                 // Filtramos todos los que son nodos de tipo elemento
                 if (nodeContacto.getNodeType() == Node.ELEMENT_NODE) {
                     Element elementContacto = (Element) nodeContacto;
                     System.out.println("Nombre del contacto: " + elementContacto.getTextContent());
+                    if (nodeContactoap.getNodeType() == Node.ELEMENT_NODE) {
+                        Element elementContacto1 = (Element) nodeContactoap;
+                        System.out.println("Apellido del contacto: " + elementContacto1.getTextContent());
+                        if (nodeContactodic.getNodeType() == Node.ELEMENT_NODE) {
+                            Element elementContacto2 = (Element) nodeContactodic;
+                            System.out.println("Dirección del contacto: " + elementContacto2.getTextContent());
+                        }
+                    }
                 }
+                System.out.println("-----------");
             }
         } catch(Exception e) { //No es buena práctica lanzar Exception, ya que no va a dar la información perteneciente a la verdadera excepción que es.
             e.printStackTrace();
--- src/Ejercicios_Clase/EjerciciosDiapositiva/Ejercicio11/Ejercicio11Escritura.java
+++ src/Ejercicios_Clase/EjerciciosDiapositiva/Ejercicio11/Ejercicio11Escritura.java
@@ -18,37 +18,108 @@ public class Ejercicio11Escritura {
         // En este caso lo hago todo en una sola línea
         Document document = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder().newDocument();
 
-        // Creamos el nodo raíz
+        // Creamos el nodo raíz <contactos></contactos>
         Element contactos= document.createElement("contactos");
         // Hacemos que cuelgue del documento (estructura de árbol)
         document.appendChild(contactos);
 
-        // Creamos el primer nodo y lo colgamos de su padre, el nodo raíz. --> <estudiante></estudiante>
+        // Creamos el primer nodo y lo colgamos de su padre, el nodo raíz. --> <contacto></contacto>
         Element contacto = document.createElement("contacto");
         contactos.appendChild(contacto);
+        //Estos dos nuevos nodos contacto2 y 3 son pruebas para intentar conseguir la separación en cada nodo contacto correcto y que no aparezcan todos en un solo nodo
+        Element contacto2 = document.createElement("contacto");
+        contactos.appendChild(contacto);
+        Element contacto3 = document.createElement("contacto");
+        contactos.appendChild(contacto);
 
+        //-----------------------------------------------------------------------
+
+        //Creamos un nodo hijo de contacto que será <nombre></nombre>
         Element nombreContacto = document.createElement("nombre");
         contacto.appendChild(nombreContacto);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text fran = document.createTextNode("Fran Reyes Lopez");
+        // y lo colgamos del nodo anterior --> <nombre>Fran Reyes Lopez</nombre>
+        nombreContacto.appendChild(fran);
 
-        // Creamos un nodo de texto que será el valor del elemento anterior
-        Text fran = document.createTextNode("Fran");
-        // y lo colgamos del nodo anterior --> <estudiante>Fran</estudiante>
-        estudianteFran.appendChild(fran);
+        //Creamos un nodo hijo de contacto que será <direccion></direccion>
+        Element direccionContacto = document.createElement("direccion");
+        contacto.appendChild(direccionContacto);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text direccionFran = document.createTextNode("Calle Zurbarán");
+        // y lo colgamos del nodo anterior
+        direccionContacto.appendChild(direccionFran);
 
-        // Igual para el siguiente estudiante
-        Element estudianteMenganito = document.createElement("estudiante");
-        estudiantes.appendChild(estudianteMenganito);
-        Text menganito = document.createTextNode("Menganito");
-        estudianteMenganito.appendChild(menganito);
+        //Creamos un nodo hijo de contacto que será <telefono></telefono>
+        Element numeroContacto = document.createElement("telefono");
+        contacto.appendChild(numeroContacto);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text numeroFran = document.createTextNode("602043332");
+        // y lo colgamos del nodo anterior --> <nombre>Fran Reyes Lopez</nombre>
+        numeroContacto.appendChild(numeroFran);
+
+        //------------------------------------------------------------------------
+
+        //Creamos un nodo hijo de contacto que será <nombre></nombre>
+        Element nombreContacto2 = document.createElement("nombre");
+        contacto2.appendChild(nombreContacto2);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text julian = document.createTextNode("Julian Moreno Franco");
+        // y lo colgamos del nodo anterior --> <nombre>Fran Reyes Lopez</nombre>
+        nombreContacto2.appendChild(julian);
+
+        //Creamos un nodo hijo de contacto que será <direccion></direccion>
+        Element direccionContacto2 = document.createElement("direccion");
+        contacto2.appendChild(direccionContacto2);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text direccionJulian = document.createTextNode("Calle Pintor Greco");
+        // y lo colgamos del nodo anterior
+        direccionContacto2.appendChild(direccionJulian);
+
+        //Creamos un nodo hijo de contacto que será <telefono></telefono>
+        Element numeroContacto2 = document.createElement("telefono");
+        contacto2.appendChild(numeroContacto2);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text numeroJulian = document.createTextNode("622443332");
+        // y lo colgamos del nodo anterior
+        numeroContacto2.appendChild(numeroJulian);
+
+        //-------------------------------------------------------------------------
+
+        //Creamos un nodo hijo de contacto que será <nombre></nombre>
+        Element nombreContacto3 = document.createElement("nombre");
+        contacto3.appendChild(nombreContacto3);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text juan = document.createTextNode("Juan Carlos Moreno");
+        // y lo colgamos del nodo anterior
+        nombreContacto3.appendChild(juan);
+
+        //Creamos un nodo hijo de contacto que será <direccion></direccion>
+        Element direccionContacto3 = document.createElement("direccion");
+        contacto3.appendChild(direccionContacto3);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text direccionJuan = document.createTextNode("Calle Pintor Velazquez");
+        // y lo colgamos del nodo anterior
+        direccionContacto3.appendChild(direccionJuan);
+
+        //Creamos un nodo hijo de contacto que será <telefono></telefono>
+        Element numeroContacto3 = document.createElement("telefono");
+        contacto3.appendChild(numeroContacto3);
+        // Creamos un nodo de texto que será el valor del elemento anterior nombre
+        Text numeroJuan = document.createTextNode("7224344332");
+        // y lo colgamos del nodo anterior --> <nombre>Fran Reyes Lopez</nombre>
+        numeroContacto3.appendChild(numeroJuan);
+
+        //---------------------------------------------------------------------------
 
         // Clases necesarias para finalizar la creación del archivo XML
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         DOMSource source = new DOMSource(document);
-        StreamResult result = new StreamResult(new File("Ejercicios_Clase/XML/ejemplo.xml"));
+        //He tenido que usar la ruta absoluta, ya que mediante la ruta del documento no me lo encontraba y me daba error por ruta inespecífica
+        StreamResult result = new StreamResult(new File("C:\\Users\\pablo\\Desktop\\Tema7\\src\\Ejercicios_Clase\\EjerciciosDiapositiva\\Ejercicio11\\contactosDePablo.xml"));
 
         // Se realiza la transformación, de Document a Fichero.
         transformer.transform(source, result);
     }
-
 }
